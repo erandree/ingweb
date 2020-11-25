f4443e3c7cd6c0f1effdf57ee919a4156745df1a
erandree <ernesto_031@hotmail.com>
2020-11-24 19:10:41 -0500

    d

--- inicio.php
+++ inicio.php
@@ -5,6 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Servicio Social Universitario</title>
   <link rel="stylesheet" href="header.css">
+  <link rel="stylesheet" href="footer.css">
   <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
   <script>
         $(function(){
