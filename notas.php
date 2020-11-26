447db107cbcc788f720c3ed573fd2f8ac15b97d7
erandree <ernesto_031@hotmail.com>
2020-11-26 11:32:05 -0500

    Cambios

---
+++ notas.php
@@ -0,0 +1,36 @@
+<?php
+
+//****************************************CONEXIÓN  
+/*
+$servername = "localhost";
+$username = "root";
+$password = "";
+//Crear Conexion con MYSQL
+$conn = new mysqli($servername, $username, $password);
+//Comprobar la Conexión
+if ($conn->connect_error) {
+    die("Fallo de Conexión: " . $conn->connect_error);
+} 
+
+echo "Conexión exitosa"; 
+
+*/
+
+
+
+
+//*******************************CREAR TABLA
+/*
+    $sql = "CREATE DATABASE [nombre]";
+    if($conn->query($sql) === true)
+    {
+        echo "Base de datos creada";
+    }
+    else
+    {
+        die("Error al crear BD: ". $conn->error);
+
+    } 
+*/
+
+?>
